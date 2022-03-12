3310f912ab9d1212e54c7e36974885e8f1c67298
Nathanael Weber <nwp8nice@rack.farm>
2022-03-12 22:34:05 +0100

    feat: solve excercise

---
+++ doc-finder/src/test/java/ch/fhnw/apm/docfinder/DocFinderBenchmarks.java
@@ -0,0 +1,57 @@
+package ch.fhnw.apm.docfinder;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+@State(Scope.Benchmark)
+public class DocFinderBenchmarks {
+    public static final String SEARCH_TEXT = "friend";
+
+    @Param({"1", "7"})
+    public int threads;
+
+    @Param({"true", "false"})
+    public boolean ignoreCase;
+
+    @Param({"friend", "hello friend world"})
+    public String searchText;
+
+    private DocFinder docFinder;
+
+    @Setup
+    public void setup() {
+        var booksDir = Path.of("doc-finder/perf-tests/books").toAbsolutePath();
+        if (!Files.isDirectory(booksDir)) {
+            System.err.println("Directory perf-tests/books not found. " +
+                    "Make sure to run this program in the doc-finder directory.");
+            System.exit(1);
+        }
+
+        docFinder = new DocFinder(booksDir);
+        docFinder.setIgnoreCase(ignoreCase);
+    }
+
+    @Benchmark
+    // @BenchmarkMode(Mode.SampleTime)
+    // @Warmup(iterations = 2)
+    // @Measurement(iterations = 3, time = 5)
+    public List<Result> findDocs() throws IOException {
+        return docFinder.findDocs(searchText, threads);
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(DocFinderBenchmarks.class.getSimpleName())
+                .forks(1)
+                .build();
+        new Runner(opt).run();
+    }
+}
